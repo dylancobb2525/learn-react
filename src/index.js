e5199987897ddcc43b7ae53fe9439ea99d9ac2cd
dylancobb2525 <dylancobb2525@gmail.com>
2025-10-16 12:36:15 -0400

    Initial React app setup with create-react-app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello Dylan</h1>
+    <h2>This is a second heading</h2>
+  </React.StrictMode>
+);
