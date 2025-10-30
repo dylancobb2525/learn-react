37655935e296d1d5081ad3608b35f19ec9379b5f
dylancobb2525 <dylancobb2525@gmail.com>
2025-10-30 18:42:27 -0400

    Fixed Slideshow component with useState and navigation, fixed Contact page syntax

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,48 @@
 import "./../css/Contact.css";
+import { useState } from "react";
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+  
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "27101fda-dbb2-4b12-b0d5-990a46122157");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+  
     return (
-        <main id="contact" className="main-content">
-            <h2>Contact</h2>
-
-        </main>
-    )
-};
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type="text" name="name" required/>
+          <input type="email" name="email" required/>
+          <textarea name="message" required></textarea>
+  
+          <button type="submit">Submit Form</button>
+  
+        </form>
+        <span>{result}</span>
+  
+      </div>
+    );
+  };
 
 export default Contact;
+
+
